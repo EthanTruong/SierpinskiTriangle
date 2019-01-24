7c0b7eb8651e6f75b486eddcdc8b69109b1368fe
ettruong <ettruong@s.sfusd.edu>
2019-01-24 13:04:27 -0800

    finished!

---
+++ out/source/SierpinskiTriangle.java
@@ -0,0 +1,46 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class SierpinskiTriangle extends PApplet {
+
+public void setup() {
+    
+    background(160,0,75);
+}
+
+public void draw() {
+    sierpinski(50,235,200);
+}
+
+public void sierpinski(int x, int y, int len) {
+    if (len <= 20) {
+        noStroke();
+        fill(255);
+        triangle(x,y,x+len,y,x+len/2,y-len);
+    } else {
+        sierpinski(x,y,len/2);
+		sierpinski(x+len/2,y,len/2);
+		sierpinski(x+len/4,y-len/2,len/2);
+    }  
+}
+  public void settings() {  size(300, 300); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "SierpinskiTriangle" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
