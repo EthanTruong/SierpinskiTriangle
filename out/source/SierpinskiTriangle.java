63c67e5d360bbe1636ba3da717830a0930d2acb5
ettruong <ettruong@s.sfusd.edu>
2019-01-25 13:36:58 -0800

    fweaf

--- out/source/SierpinskiTriangle.java
+++ out/source/SierpinskiTriangle.java
@@ -29,6 +29,7 @@ public void sierpinski(int x, int y, int len) {
         fill(255);
         triangle(x,y,x+len,y,x+len/2,y-len);
     } else {
+        delay(1000);
         sierpinski(x,y,len/2);
 		sierpinski(x+len/2,y,len/2);
 		sierpinski(x+len/4,y-len/2,len/2);
